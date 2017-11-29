3f738c472558ae27ba6e300084d4f747dd3a90c0
JAMES WINTER <34040@actslife.org>
2017-11-29 16:02:36 -0500

    Create classes_dates migration file. Modify related migrations.

--- database/migrations/2017_10_12_131857_create_class_types_table.php
+++ database/migrations/2017_10_12_131857_create_class_types_table.php
@@ -18,11 +18,45 @@ class CreateClassTypesTable extends Migration
             $table->string('Name', 100)->nullable();
             $table->integer('ProrateOnEnrollment')->nullable();
             $table->integer('ChargeRegistrationFee')->nullable();
-            $table->integer('rate_id');
+            $table->integer('rate_group_id');
             $table->integer('created_by')->nullable();
             $table->integer('updated_by')->nullable();
             $table->timestamps();
         });
+
+        DB::table('class_types')->insert([
+            [
+                'Name' => 'Ballet',
+                'ProrateOnEnrollment' => '2',
+                'ChargeRegistrationFee' => '1',
+                'rate_group_id' => '1'
+            ],
+            [
+                'Name' => 'Jazz',
+                'ProrateOnEnrollment' => '2',
+                'ChargeRegistrationFee' => '1',
+                'rate_group_id' => '1'
+            ],
+            [
+                'Name' => 'Modern',
+                'ProrateOnEnrollment' => '2',
+                'ChargeRegistrationFee' => '1',
+                'rate_group_id' => '1'
+            ],
+            [
+                'Name' => 'Tap',
+                'ProrateOnEnrollment' => '2',
+                'ChargeRegistrationFee' => '1',
+                'rate_group_id' => '1'
+            ],
+            [
+                'Name' => 'Lyrical',
+                'ProrateOnEnrollment' => '2',
+                'ChargeRegistrationFee' => '1',
+                'rate_group_id' => '1'
+
+            ]
+        ]);
     }
 
     /**
