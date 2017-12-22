fa8622a2fd35e9445670ccae653405aedfe1ab21
JAMES WINTER <34040@actslife.org>
2017-12-22 13:24:30 -0500

    Class display changes; change layout for master and nav.

--- resources/views/classes/index.blade.php
+++ resources/views/classes/index.blade.php
@@ -1,35 +1,43 @@
 @extends('layouts.master')
 
 @section('content')
-	<div class="tabs is-boxed">
-		<ul>
-		@foreach ($seasons as $season)
-			<li><a href="/classes/season/{{$season->id}}">{{$season->Name}}</a></li>
-		@endforeach
-		</ul>
-	</div>
-	@if ($season->SeasonType == 1)
-		<div class="columns">
-		@php
-		$daysOfWeek = array('Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday')
-		@endphp
-		@foreach ($daysOfWeek as $day)
-			<div class="column">
-				<h6 class="subtitle is-6">{{$day}}</h6>
-			@foreach ($classes as $class)
-				@if ($class->DayHeldOn == $day)
-					<p class="is-size-7"><a href="/classes/{{$class->id}}">{{ $class->Name}}</a></p>
-					<p class="is-size-7"><i class="fa fa-clock-o" aria-hidden="true"></i> {{$class->StartTime}}</p>
-					<p class="is-size-7"><i class="fa fa-user-circle" aria-hidden="true"></i> {{$class->instructor->Display}}</p>
-					<p class="is-size-7"><i class="fa fa-map-marker" aria-hidden="true"></i> {{$class->location->Type}}</p>
-					<p class="is-size-7"><i class="fa fa-user" aria-hidden="true"></i> Ages {{$class->AgeFrom}} to {{$class->AgeTo}}</p>
-					<hr/>
+	<div class="card">
+		<div class="card-content">
+			<div class="content">
+				<div class="tabs">
+					<ul>
+					@foreach ($seasons as $seasonTemp)
+						<li @if ($season->id == $seasonTemp->id) class="is-active" @endif><a href="/classes/season/{{$seasonTemp->id}}">{{$seasonTemp->Name}}</a></li>
+					@endforeach
+					</ul>
+				</div>
+				@if ($season->SeasonType === 1)
+					<div class="columns">
+					@php
+					$daysOfWeek = array('Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday')
+					@endphp
+					@foreach ($daysOfWeek as $day)
+						<div class="column">
+							<h6 class="subtitle is-6"><em>{{$day}}</em></h6>
+						@foreach ($classes as $class)
+							@if ($class->DayHeldOn == $day)
+								<p class="is-size-6 is-paddingless is-marginless"><a href="/classes/{{$class->id}}">{{ $class->Name}}</a></p>
+								<p class="is-size-7 is-paddingless is-marginless">
+									<span style="display:inline-block;"><i class="fa fa-clock-o" aria-hidden="true"></i> {{$class->StartTime}} ({{$class->Length}} min.)</span> /
+									<span style="display:inline-block;"><i class="fa fa-user-circle" aria-hidden="true"></i> {{$class->instructor->Display}}</span> /
+									<span style="display:inline-block;"><i class="fa fa-map-marker" aria-hidden="true"></i> {{$class->location->Type}}</span> /
+									<span style="display:inline-block;"><i class="fa fa-user" aria-hidden="true"></i> Ages {{$class->AgeFrom}} to {{$class->AgeTo}}</span>
+								</p>
+								<hr/>
+							@endif
+						@endforeach
+						</div>
+					@endforeach
+					</div>
+				@elseif ($season->SeasonType === 2)
+					Date-Specific Placeholder
 				@endif
-			@endforeach
 			</div>
-		@endforeach
 		</div>
-	@elseif ($season->SeasonType == 2)
-
-	@endif
+	</div>
 @endsection
---
+++ resources/views/layouts/sidenav.blade.php
@@ -0,0 +1,31 @@
+<aside class="menu">
+    <p class="menu-label">
+        General
+    </p>
+    <ul class="menu-list">
+        <li><a href="/classes">Classes</a></li>
+        <li><a href="/families">Families</a></li>
+        <li><a href="/students">Students</a></li>
+        <li><a href="/instructors">Instructors</a></li>
+        <li><a href="/payments">Payments</a></li>
+        <li><a href="/reports">Reports</a></li>
+    </ul>
+    <p class="menu-label">
+        Administration
+    </p>
+    <ul class="menu-list">
+        <li><a href="/seasons/">Seasons</a></li>
+        <li><a href="/class-types/">Class Types</a></li>
+        <li><a href="/class-locations/">Class Locations</a></li>
+        <li><a href="/auto-tuition-rates/">Auto Tuition Rates</a></li>
+        <li><a href="/admin/tables">Lookup Tables</a></li>
+    </ul>
+    <!--<p class="menu-label">
+        Transactions
+    </p>
+    <ul class="menu-list">
+        <li><a>Payments</a></li>
+        <li><a>Transfers</a></li>
+        <li><a>Balance</a></li>
+    </ul>-->
+</aside>
