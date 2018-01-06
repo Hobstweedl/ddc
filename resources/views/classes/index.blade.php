a3769b1d6b6057bd7211f4f073b405813d6b6fbf
Jesse Rice <hobstweedl@gmail.com>
2018-01-06 01:54:28 -0500

    clean up classes views

--- resources/views/classes/index.blade.php
+++ resources/views/classes/index.blade.php
@@ -7,8 +7,8 @@
 				<div class="tabs">
 					<ul>
 					@foreach ($seasons as $seasonTemp)
-						<li @if ($season->id == $seasonTemp->id) class="is-active" @endif>
-							<a href="/classes/season/{{$seasonTemp->id}}">{{$seasonTemp->Name}}</a>
+						<li {{ $season->id === $seasonTemp->id ? 'class="is-active"' : '' }}>
+							<a href="{{ route('classes.season', $seasonTemp->id) }}">{{$seasonTemp->Name}}</a>
 						</li>
 					@endforeach
 					</ul>
@@ -21,7 +21,7 @@
 						@foreach ($classes as $class)
 							@if ($class->DayHeldOn == $day)
 								<p class="is-size-6 is-paddingless is-marginless">
-									<a href="/classes/{{$class->id}}">{{ $class->Name}}</a>
+									<a href="{{ route('classes.show', $chass->id) }}">{{ $class->Name}}</a>
 								</p>
 								<p class="is-size-7 is-paddingless is-marginless">
 									<span style="display:inline-block;">
@@ -61,11 +61,22 @@
 						@if ($i == 1)
 						<div class="column">
 						@endif
-							<p class="is-size-6 is-paddingless is-marginless"><a href="/classes/{{$class->id}}">{{ $class->Name}}</a></p>
+							<p class="is-size-6 is-paddingless is-marginless">
+								<a href="{{ route('classes.show', $class->id) }}">{{ $class->Name}}</a>
+							</p>
 							<p class="is-size-7 is-paddingless is-marginless">
-								<span style="display:inline-block;"><i class="fa fa-user-circle" aria-hidden="true"></i> {{$class->instructor->Display}}</span> /
-								<span style="display:inline-block;"><i class="fa fa-map-marker" aria-hidden="true"></i> {{$class->location->Type}}</span> /
-								<span style="display:inline-block;"><i class="fa fa-user" aria-hidden="true"></i> Ages {{$class->AgeFrom}} to {{$class->AgeTo}}</span>
+								<span style="display:inline-block;">
+									<i class="fa fa-user-circle" aria-hidden="true"></i>
+									{{$class->instructor->Display}}
+								</span> /
+								<span style="display:inline-block;">
+									<i class="fa fa-map-marker" aria-hidden="true"></i>
+									{{$class->location->Type}}
+								</span> /
+								<span style="display:inline-block;">
+									<i class="fa fa-user" aria-hidden="true"></i>
+									Ages {{$class->AgeFrom}} to {{$class->AgeTo}}
+								</span>
 								@foreach ($class->dates as $classdate)
 									<p class="is-size-7 is-paddingless is-marginless">
 										<i class="fa fa-calendar-check-o" aria-hidden="true"></i>
