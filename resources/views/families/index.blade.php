fddd471b834a52c146d96dbca118fe6fc113e175
winterj <winterj@gmail.com>
2018-01-24 19:06:27 +0000

    Updated to laravel 5.6. Added container div for families.index

--- resources/views/families/index.blade.php
+++ resources/views/families/index.blade.php
@@ -7,46 +7,48 @@
 			@include('families.list')
 		</div>
 		<div class="column auto">
-			<div class="columns is-multiline is-mobile">
-				@if (isset($family))	
-					<div class="column is-half">
-						<div class="box">
-							@include('families.create', $family)
-						</div>
-					</div>
-					<div class="column is-half">
-						<div class="box">
-							<h5 class="title is-5"><i class="fa fa-user" aria-hidden="true"></i> Students</h5>
-							@include('students.list', ['students' => $family->students])
-							<div class="control">
-								<a class="button is-primary" href="#">Add Student</a>
-							</div>
-						</div>
-						<div class="box">
-							<h5 class="title is-5"><i class="fa fa-sticky-note-o" aria-hidden="true"></i> Notes</h5>
-							@foreach($family->notes as $note)
-								{{$note->Content}}
-							@endforeach
-							<div class="control">
-								<a class="button is-primary" href="#">Add Note</a>
-							</div>
-						</div>
-					</div>
-				@else
-					<div class="column">
-						<div class="box">
-							@include('families.create')
-						</div>
-					</div>
-				@endif
-				@if (isset($family))
-				<div class="column auto">
-					<div class="box">
-						<h5 class="title is-5"><i class="fa fa-money" aria-hidden="true"></i> Account</h5>
-					</div>
-				</div>	
-				@endif
-			</div>
+			<div class="container">
+        <div class="columns is-multiline">
+          @if (isset($family))	
+            <div class="column is-half">
+              <div class="box">
+                @include('families.create', $family)
+              </div>
+            </div>
+            <div class="column is-half">
+              <div class="box">
+                <h5 class="title is-5"><i class="fa fa-user" aria-hidden="true"></i> Students</h5>
+                @include('students.list', ['students' => $family->students])
+                <div class="control">
+                  <a class="button is-primary" href="#">Add Student</a>
+                </div>
+              </div>
+              <div class="box">
+                <h5 class="title is-5"><i class="fa fa-sticky-note-o" aria-hidden="true"></i> Notes</h5>
+                @foreach($family->notes as $note)
+                  {{$note->Content}}
+                @endforeach
+                <div class="control">
+                  <a class="button is-primary" href="#">Add Note</a>
+                </div>
+              </div>
+            </div>
+          @else
+            <div class="column">
+              <div class="box">
+                @include('families.create')
+              </div>
+            </div>
+          @endif
+          @if (isset($family))
+          <div class="column auto">
+            <div class="box">
+              <h5 class="title is-5"><i class="fa fa-money" aria-hidden="true"></i> Account</h5>
+            </div>
+          </div>	
+          @endif
+        </div>
+      </div>
 		</div>
 	</div>
 @endsection
