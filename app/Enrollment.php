cd8eea59a953ebd00c9fd45bb2b32b9b8cf96628
winterj <winterj@gmail.com>
2018-02-09 17:03:21 -0500

    Wrap columns next to list in container div, list enrollments for student. (#46)

--- app/Enrollment.php
+++ app/Enrollment.php
@@ -3,8 +3,20 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Enrollment extends Model
 {
-    //
+    public function class() {
+        return $this->belongsTo(Classes::class, 'classes_id');
+    }
+
+    public function date() {
+        return $this->belongsTo(ClassDate::class, 'class_dates_id');
+    }
+
+    public function friendlyDate($date)
+    {
+        return (Carbon::parse($date)->format('D\, M\. jS Y'));
+    }
 }
